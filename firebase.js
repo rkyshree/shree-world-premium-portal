b952ab92c9e4aa31f98c96933f97de6df2b4a974
ʀ.ᴋ. ʏᴀᴅᴀᴠ <rkyshree.in@gmail.com>
2026-07-26 20:29:07 +0530

    Add Firebase configuration to firebase.js

---
+++ firebase.js
@@ -0,0 +1,9 @@
+{
+  apiKey: "AIzaSyCOMp4Z4XncCXCLiKb3OwsqkAMe2oFSd2I",
+  authDomain: "rkyshree-2f74b.firebaseapp.com",
+  projectId: "rkyshree-2f74b",
+  storageBucket: "rkyshree-2f74b.firebasestorage.app",
+  messagingSenderId: "354823113610",
+  appId: "1:354823113610:web:d043805305c945339c9673",
+  measurementId: "G-WSXGTGFWYK"
+};
